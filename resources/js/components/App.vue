4d74221bf1aa3a94d959c9d529bf915a908d56af
Jorge Moura <jjorgeammoura@gmail.com>
2022-07-17 14:33:19 -0300

    First Commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-white">
+        Laravel 9 {{ vue3 }} Vite <span class="font-bold">Tailwind</span>
+    </div>
+</template>
+
+<script setup>
+    let vue3 = 'Vue3';
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Morera LTDA</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="h-full grid bg-slate-600 place-items-center">
+    <div id="app"></div>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
